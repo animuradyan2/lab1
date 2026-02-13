783513e9805b73b8096b6a186dc6aea039b6c9b1
animuradyan2 <ani.muradyan2@tumo.org>
2026-02-13 10:32:36 +0400

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+
+    cout << "Մուտքագրեք առաջին թիվը: ";
+    cin >> a;
+
+    cout << "Մուտքագրեք երկրորդ թիվը: ";
+    cin >> b;
+
+    double sum = a + b;
+
+    cout << "Գումարը հավասար է: " << sum << endl;
+
+    return 0;
+}
